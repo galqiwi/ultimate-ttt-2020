2d928e5a974b5523268deaa5754d32019d5ed03a
Vladimir Malinovskii <galqiwi@galqiwi.ru>
2022-05-22 19:42:57 +0300

    simple_strategy

--- strategy/Strategy.cpp
+++ strategy/Strategy.cpp
@@ -2,5 +2,16 @@
 
 
 std::pair<int, int> Strategy::GetNextMove(std::optional<std::pair<int, int>> enemyMove) {
-    return {-1, -1};
+    if (enemyMove) {
+        Game_.MakeMove(Players::First, enemyMove->first, enemyMove->second);
+    }
+    for (int x = 0; x < 9; ++x) {
+        for (int y = 0; y < 9; ++y) {
+            if (Game_.CanMakeMove(Players::Second, x, y)) {
+                Game_.MakeMove(Players::Second, x, y);
+                return {x, y};
+            }
+        }
+    }
+    abort();
 }
--- strategy/Strategy.h
+++ strategy/Strategy.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "../runner/game_state.h"
 #include <optional>
 #include <utility>
 
 class Strategy {
 public:
     std::pair<int, int> GetNextMove(std::optional<std::pair<int, int>> enemyMove);
+private:
+    GameState Game_;
 };
